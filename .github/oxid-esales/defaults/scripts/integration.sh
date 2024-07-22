2bbe7750b30dbccfaf87a48ef084a0f2b4276372
Jörn Ott <joern.ott@ott-consult.de>
2024-07-22 11:25:46 +0200

    Fix integration

--- .github/oxid-esales/defaults/scripts/integration.sh
+++ .github/oxid-esales/defaults/scripts/integration.sh
@@ -2,82 +2,74 @@
 set -e
 set -x
 export XDEBUG_MODE=coverage
+function init() {
+    # shellcheck disable=SC2128
+    if [[ ${BASH_SOURCE} = */* ]]; then
+        SCRIPT_DIR=${BASH_SOURCE%/*}/
+    else
+        SCRIPT_DIR=./
+    fi
+    if [ -z "${ABSOLUTE_PATH}" ]; then
+        ABSOLUTE_PATH="$(pwd)"
+    else
+        ABSOLUTE_PATH="/var/www/${ABSOLUTE_PATH}"
+    fi
 
-if [ -z "${ABSOLUTE_PATH}" ]; then
-    ABSOLUTE_PATH="$(pwd)"
-else
-    ABSOLUTE_PATH="/var/www/${ABSOLUTE_PATH}"
-fi
+    [[ ! -d "${ABSOLUTE_PATH}/tests/Output" ]] && mkdir "${ABSOLUTE_PATH}/tests/Output"
+    [[ ! -d "${ABSOLUTE_PATH}/tests/Output" ]] && mkdir "${ABSOLUTE_PATH}/tests/Reports"
 
-[[ ! -d "${ABSOLUTE_PATH}/tests/Output" ]] && mkdir "${ABSOLUTE_PATH}/tests/Output"
-[[ ! -d "${ABSOLUTE_PATH}/tests/Reports" ]] && mkdir "${ABSOLUTE_PATH}/tests/Reports"
+    OUTPUT_DIR="${ABSOLUTE_PATH}/tests/Output"
+    REPORT_DIR="${ABSOLUTE_PATH}/tests/Reports"
 
-if [ -z "${SUITE}" ]; then
-    SUITE="${ABSOLUTE_PATH}/tests/Integration"
-fi
+    if [ -z "${SUITE}" ]; then
+        SUITE="${ABSOLUTE_PATH}/tests/Integration"
+    fi
 
-PHPUNIT="vendor/bin/phpunit"
-if [ ! -f "${PHPUNIT}" ]; then
-    PHPUNIT="/var/www/${PHPUNIT}"
+    LOG_FILE="${OUTPUT_DIR}/integration_${SUITE}.txt"
+    PATTERN_FILE="${SCRIPT_DIR}/integration_failure_pattern.txt"
+
+    PHPUNIT="vendor/bin/phpunit"
     if [ ! -f "${PHPUNIT}" ]; then
-        echo -e "\033[0;31mCould not find phpunit in vendor/bin or /var/www/vendor/bin\033[0m"
-        exit 1
-    fi
-fi
-BOOTSTRAP="/var/www/source/bootstrap.php"
-if [ ! -f "${BOOTSTRAP}" ]; then
-    BOOTSTRAP="/var/www/vendor/oxid-esales/oxideshop-ce/tests/bootstrap.php"
-    if [ ! -f "${BOOTSTRAP}" ]; then
-        echo -e "\033[0;31mCould not find bootstrap.php in /var/www/tests or /var/www/oxid-esales/oxideshop-ce/tests\033[0m"
-        find /var/www -iname "bootstrap.php"
-        exit 1
-    fi
-fi
-"${PHPUNIT}" \
-    -c "${ABSOLUTE_PATH}/tests/phpunit.xml" \
-    --bootstrap "${BOOTSTRAP}" \
-    --coverage-clover="${ABSOLUTE_PATH}/tests/Reports/coverage_phpunit_integration.xml" \
-    "${SUITE}" 2>&1 \
-| tee "${ABSOLUTE_PATH}/tests/Output/integration_tests.txt"
-RESULT=$?
-echo "phpunit exited with error code ${RESULT}"
-if [ ! -s "${ABSOLUTE_PATH}/tests/Output/integration_tests.txt" ]; then
-    echo -e "\033[0;31mLog file is empty! Seems like no tests have been run!\033[0m"
-    RESULT=1
-fi
-cat >"${ABSOLUTE_PATH}/integration_failure_pattern.tmp" <<EOF
-fail
-\\.\\=\\=
-Warning
-Notice
-Deprecated
-Fatal
-Error
-DID NOT FINISH
-Test file ".+" not found
-Cannot open file
-No tests executed
-Could not read
-Warnings: [1-9][0-9]*
-Errors: [1-9][0-9]*
-Failed: [1-9][0-9]*
-Deprecations: [1-9][0-9]*
-Risky: [1-9][0-9]*
-EOF
-sed -e 's|(.*)\r|$1|' -i "${ABSOLUTE_PATH}/integration_failure_pattern.tmp"
-while read -r LINE ; do
-    if [ -n "${LINE}" ]; then
-        if grep -q -E "${LINE}" "${ABSOLUTE_PATH}/tests/Output/integration_tests.txt"; then
-            echo -e "\033[0;31m integration test failed matching pattern ${LINE}\033[0m"
-            grep -E "${LINE}" "${ABSOLUTE_PATH}/tests/Output/integration_tests.txt"
-            RESULT=1
-        else
-            echo -e "\033[0;32m integration test passed matching pattern ${LINE}"
+        PHPUNIT="/var/www/${PHPUNIT}"
+        if [ ! -f "${PHPUNIT}" ]; then
+            echo -e "\033[0;31mCould not find phpunit in vendor/bin or /var/www/vendor/bin\033[0m"
+            exit 1
         fi
     fi
-done <"${ABSOLUTE_PATH}/integration_failure_pattern.tmp"
-if [[ ! -s "${ABSOLUTE_PATH}/tests/Reports/coverage_phpunit_integration.xml" ]]; then
-    echo -e "\033[0;31m coverage report ${ABSOLUTE_PATH}/tests/Reports/coverage_phpunit_integration.xml is empty\033[0m"
-    RESULT=1
-fi
-exit ${RESULT}
+
+    BOOTSTRAP="/var/www/source/bootstrap.php"
+    if [ ! -f "${BOOTSTRAP}" ]; then
+        BOOTSTRAP="/var/www/vendor/oxid-esales/oxideshop-ce/tests/bootstrap.php"
+        if [ ! -f "${BOOTSTRAP}" ]; then
+            echo -e "\033[0;31mCould not find bootstrap.php in /var/www/tests or /var/www/oxid-esales/oxideshop-ce/tests\033[0m"
+            find /var/www -iname "bootstrap.php"
+            exit 1
+        fi
+    fi
+
+    XML_FILE="${ABSOLUTE_PATH}/tests/phpunit.xml"
+    COVERAGE_FILE="${REPORT_DIR}/coverage_phpunit_integration.xml"
+
+    cat <<EOF
+        Path: ${ABSOLUTE_PATH}
+        Script directory: ${SCRIPT_DIR}
+        Output directory: ${OUTPUT_DIR}
+        Report directory: ${REPORT_DIR}
+        Script directory: ${SCRIPT_DIR}
+        Suite: ${SUITE}
+        Bootstrap: ${BOOTSTRAP}
+        Config: ${XML_FILE}
+        Phpunit: ${PHPUNIT}
+        Coverage: ${COVERAGE_FILE}
+        Log file: ${LOG_FILE}
+        Failure patterns: ${PATTERN_FILE}
+EOF
+}
+
+init
+
+"${PHPUNIT}" -c "${XML_FILE}" --bootstrap "${BOOTSTRAP}" --coverage-clover="${COVERAGE_FILE}" "${SUITE}" 2>&1 \
+| tee "${LOG_FILE}"
+RESULT=$?
+echo "phpunit exited with error code ${RESULT}"
+"${SCRIPT_DIR}/check_log.sh" "${LOG_FILE}" "${PATTERN_FILE}"
